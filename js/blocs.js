8352a3f7d04460d431f237c05eb4145b862e33f4
vinceny <vinceny@live.fr>
2020-12-20 19:08:01 +0300

    LaChouetteAgence

--- js/blocs.js
+++ js/blocs.js
@@ -188,6 +188,7 @@ function addLightBoxSwipeSupport() {
         threshold: 0
     })
 }
+
 $(document).ready(function() {
     $("#scroll-hero").click(function(t) {
         t.preventDefault(), $("html,body").animate({
